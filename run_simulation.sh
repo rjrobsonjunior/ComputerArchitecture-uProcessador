cde4e7ea150f2f044a7ebd5ddb4a2678609f33e6
Robson Junior <rob.santanaju@gmail.com>
2024-04-17 20:00:51 -0300

    full test bench

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,14 +1,14 @@
-# Compile vhd files
+# -a Compile vhd files 
+# -e Develop the design
 ghdl -a ULA.vhd
-ghdl -a ULA_tb.vhd
-
-# Develop the design
 ghdl -e ULA
+ghdl -a ULA_tb.vhd
 ghdl -e ULA_tb
+
 # Run the simULAtion and generate the wave file
 ghdl ULA_tb -r --wave=ULA_tb.ghw 
 
 # Open the wave file using GtkWave
-gtkwave ULA_tb.ghw
+#gtkwave ULA_tb.ghw
 
 
