1c3569b04ebf165f751c99f24c58b5e604fc3aa5
Robson Junior <rob.santanaju@gmail.com>
2024-04-17 20:09:32 -0300

    bugfix run_simulation.sh

--- run_simulation.sh
+++ run_simulation.sh
@@ -6,9 +6,9 @@ ghdl -a ULA_tb.vhd
 ghdl -e ULA_tb
 
 # Run the simULAtion and generate the wave file
-ghdl ULA_tb -r --wave=ULA_tb.ghw 
+ghdl -r ULA_tb --wave=ULA_tb.ghw 
 
 # Open the wave file using GtkWave
-#gtkwave ULA_tb.ghw
+gtkwave ULA_tb.ghw
 
 
