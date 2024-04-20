4756663b656f6bb9a3b2bc1cc54054e75f68b4ec
Robson Junior <rob.santanaju@gmail.com>
2024-04-20 14:15:57 -0300

    complete Register Bank in faith

--- RegisterBank.vhd
+++ RegisterBank.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity RegisterBank is
     port (
-        adress1, adress2, writeRegister: in std_logic_vector(2 downto 0);
+        adress1, adress2, writeRegister: in unsigned(2 downto 0);
         datawr : unsigned(15 downto 0);
         wren, clk, rst: in std_logic;
         data1, data2 : out unsigned(15 downto 0)
@@ -15,20 +15,19 @@ architecture rtl of RegisterBank is
 
     component MUX4_16 is
         port (
-        selector   : in unsigned (2 downto 0);
-        inMUX      : in unsigned ( 127 downto 0); 
-        outMUX     : out unsigned (15 downto 0) 
+            selector   : in unsigned (2 downto 0);
+            inMUX      : in unsigned ( 127 downto 0); 
+            outMUX     : out unsigned (15 downto 0) 
         );
     end component;
 
     component reg16bits is
-    port( 
-            clk      : in std_logic;
-            rst      : in std_logic;
-            wr_en    : in std_logic;
-            data_in  : in unsigned(15 downto 0);
-            data_out : out unsigned(15 downto 0)
-    ); 
+        port( clk      : in std_logic;
+         rst      : in std_logic;
+         wr_en    : in std_logic;
+         data_in  : in unsigned(15 downto 0);
+         data_out : out unsigned(15 downto 0)
+   );
     end component; 
 
     signal wren_register : std_logic_vector (7 downto 0);
@@ -45,24 +44,24 @@ begin
 
     gen00:for i in 0 to 7 generate
 		register0 : reg16bits port map (
-            clk      <= clk,
-            rst      <= rst,
-            wr_en    <= wren_register(i),
-            data_in  <=  datawr,
-            data_out <=  outEightRegisters( (16*(i+1))-1 downto 16*i )   
+            clk      => clk,
+            rst      => rst,
+            wr_en    => wren_register(i),
+            data_in  =>  datawr,
+            data_out => outEightRegisters( (16*(i+1))-1 downto 16*i )   
         );   
     end generate;
 
     mux1 : MUX4_16 port map (
-        selector <= adress1,
-        inMUX    <= outEightRegisters,
-        outMUX   <= data1
+        selector => adress1,
+        inMUX    => outEightRegisters,
+        outMUX   => data1
     );
     
     mux2 : MUX4_16 port map (
-        selector <= adress2,
-        inMUX    <= eoutEightRegisters,
-        outMUX   <= data1
+        selector => adress2,
+        inMUX    => outEightRegisters,
+        outMUX   => data1
     );
 
 end architecture;
