677ef28aea05b4828c0b642260e9a467eb437492
MuriloUnten <murilokenjiunten@gmail.com>
2024-04-22 14:05:38 -0300

    fix zero register in MUX4_16

--- MUX4_16.vhd
+++ MUX4_16.vhd
@@ -13,13 +13,13 @@ end entity MUX4_16;
 architecture rtl of MUX4_16 is
 
 begin
-    outMUX <= inMUX( 31 downto 16) when selector= "001" else
+    outMUX <= X"0000" when selector = "000" else -- Register zero
+              inMUX( 31 downto 16) when selector= "001" else
               inMUX( 47 downto 32) when selector= "010" else
               inMUX( 63 downto 48) when selector= "011" else
               inMUX( 79 downto 64) when selector= "100" else
               inMUX( 95 downto 80) when selector= "101" else
               inMUX( 111 downto 96) when selector=  "110" else
-              inMUX( 127 downto 112) when selector= "111" else
-              X"0000";
+              inMUX( 127 downto 112) when selector= "111";
 
 end architecture;
