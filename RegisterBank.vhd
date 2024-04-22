5b4ef24b347cd45b888039cad534dc58e7cbddd7
Murilo Unten <murilo.unten@proton.me>
2024-04-22 19:29:04 -0300

    work on regbank fix, still not working

--- RegisterBank.vhd
+++ RegisterBank.vhd
@@ -4,8 +4,8 @@ use ieee.numeric_std.all;
 
 entity RegisterBank is
     port (
-        adress1, adress2, writeRegister: in unsigned(2 downto 0);
-        datawr : unsigned(15 downto 0);
+        address1, address2, writeRegister: in unsigned(2 downto 0);
+        datawr : in unsigned(15 downto 0);
         wren, clk, rst: in std_logic;
         data1, data2 : out unsigned(15 downto 0)
     );
@@ -32,8 +32,10 @@ architecture rtl of RegisterBank is
 
     signal wren_register : std_logic_vector (7 downto 0);
     signal outEightRegisters: unsigned ( 127 downto 0);
+    type t_registers is array (natural range <>) of reg16bits;
+    signal registers : t_registers(7 downto 0);
 begin
-    wren_register(0) <= '0'; --0
+    wren_register(0) <= '0'; -- Register zero cannot be written to
     wren_register(1) <= (not writeRegister(0)) and (not writeRegister(1)) and ( writeRegister(2));    --1
     wren_register(2) <= (not writeRegister(0)) and ( writeRegister(1)) and (not writeRegister(2));    --2
     wren_register(3) <= (not writeRegister(0)) and (writeRegister(1)) and (writeRegister(2));         --3
@@ -43,25 +45,26 @@ begin
     wren_register(7) <= (writeRegister(0)) and (writeRegister(1)) and (writeRegister(2));             --7
 
     gen00:for i in 0 to 7 generate
-		register0 : reg16bits port map (
+    begin
+		registers(i) <= reg16bits port map (
             clk      => clk,
             rst      => rst,
             wr_en    => wren_register(i),
             data_in  =>  datawr,
             data_out => outEightRegisters( (16*(i+1))-1 downto 16*i )   
         );   
-    end generate;
+    end generate gen00;
 
     mux1 : MUX4_16 port map (
-        selector => adress1,
+        selector => address1,
         inMUX    => outEightRegisters,
         outMUX   => data1
     );
     
     mux2 : MUX4_16 port map (
-        selector => adress2,
+        selector => address2,
         inMUX    => outEightRegisters,
-        outMUX   => data1
+        outMUX   => data2
     );
 
 end architecture;
