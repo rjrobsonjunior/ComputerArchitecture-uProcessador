68c06a24faa1c99c786d3fe4c050f6c355c3e01b
Murilo Unten <murilo.unten@proton.me>
2024-05-10 15:17:17 -0300

    add accumulator to ULA_RegBank and fix tb

--- ULA_RegBank.vhd
+++ ULA_RegBank.vhd
@@ -5,19 +5,22 @@ use ieee.numeric_std.all;
 entity ULA_RegBank is
     port (
             clk, rst, wr_en : in std_logic;
-            mux_selector    : in std_logic;
+            r_data_1_mux_selector    : in std_logic;
+            r_data_2_mux_selector    : in std_logic;
 
             rb_address1     : in unsigned(2 downto 0);
             rb_address2     : in unsigned(2 downto 0);
             rb_wr_reg       : in unsigned(2 downto 0);
             
-            ula_mux_input   : in unsigned(15 downto 0);
+            mux_1_input   : in unsigned(15 downto 0);
             ula_selector    : in unsigned(1 downto 0);
             ula_carry       : out std_logic;
             ula_overflow    : out std_logic;
             ula_bigger      : out std_logic;
             ula_smaller     : out std_logic;
-            ula_output      : out unsigned(15 downto 0)
+            ula_output      : out unsigned(15 downto 0);
+
+            accumulator : out unsigned(15 downto 0)
          );
 end entity ULA_RegBank;
 
@@ -55,24 +58,34 @@ architecture impl of ULA_RegBank is
 
     signal regbank_read_data1 : unsigned(15 downto 0);
     signal regbank_read_data2 : unsigned(15 downto 0);
-    signal mux_to_ula : unsigned(15 downto 0);
+    signal mux_1_to_ula : unsigned(15 downto 0);
+    signal mux_2_to_ula : unsigned(15 downto 0);
     signal ula_out_to_regbank_datawr : unsigned(15 downto 0);
     signal ula_out_signal : unsigned(15 downto 0);
+    signal accumulator_s : unsigned(15 downto 0);
 begin
     
-    mux_component: MUX2_16
+    r_data_1_mux: MUX2_16
     port map (
-                 input_signal => mux_selector,
+                 input_signal => r_data_1_mux_selector,
+                 input0 => regbank_read_data1,
+                 input1 => mux_1_input,
+                 output => mux_1_to_ula
+             );
+
+    r_data_2_mux: MUX2_16
+    port map (
+                 input_signal =>r_data_2_mux_selector,
                  input0 => regbank_read_data2,
-                 input1 => ula_mux_input,
-                 output => mux_to_ula
+                 input1 => accumulator_s,
+                 output => mux_2_to_ula
              );
 
     ula_component: ULA
     port map (
                  selector => ula_selector,
-                 inputA => regbank_read_data1,
-                 inputB => mux_to_ula,
+                 inputA => mux_1_to_ula,
+                 inputB => mux_2_to_ula,
                  outResult => ula_out_signal,
                  carry => ula_carry,
                  overflow => ula_overflow,
@@ -96,5 +109,12 @@ begin
 
     ula_output <= ula_out_signal;
 
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            accumulator_s <= ula_out_signal;
+        end if;
+    end process;
+    accumulator <= accumulator_s;
 
 end impl;
--- ULA_RegBank_tb.vhd
+++ ULA_RegBank_tb.vhd
@@ -9,19 +9,22 @@ architecture impl of ULA_RegBank_tb is
     component ULA_RegBank is
         port (
             clk, rst, wr_en : in std_logic;
-            mux_selector    : in std_logic;
+            r_data_1_mux_selector    : in std_logic;
+            r_data_2_mux_selector    : in std_logic;
 
-            rb_address2     : in unsigned(2 downto 0);
             rb_address1     : in unsigned(2 downto 0);
+            rb_address2     : in unsigned(2 downto 0);
             rb_wr_reg       : in unsigned(2 downto 0);
-
-            ula_mux_input   : in unsigned(15 downto 0);
+            
+            mux_1_input   : in unsigned(15 downto 0);
             ula_selector    : in unsigned(1 downto 0);
             ula_carry       : out std_logic;
             ula_overflow    : out std_logic;
             ula_bigger      : out std_logic;
             ula_smaller     : out std_logic;
-            ula_output      : out unsigned(15 downto 0)
+            ula_output      : out unsigned(15 downto 0);
+
+            accumulator : out unsigned(15 downto 0)
     );
     end component;
 
@@ -31,13 +34,14 @@ architecture impl of ULA_RegBank_tb is
     signal clk           : std_logic;
     signal rst           : std_logic;
     signal wr_en         : std_logic;
-    signal mux_selector  : std_logic;
+    signal r_data_1_mux_selector: std_logic;
+    signal r_data_2_mux_selector: std_logic := '0';
 
     signal rb_address1   : unsigned(2 downto 0);
     signal rb_address2   : unsigned(2 downto 0);
     signal rb_wr_reg     : unsigned(2 downto 0);
 
-    signal ula_mux_input : unsigned(15 downto 0);
+    signal mux_1_input : unsigned(15 downto 0);
     signal ula_selector  : unsigned(1 downto 0);
     signal ula_carry     : std_logic;
     signal ula_overflow  : std_logic;
@@ -51,11 +55,12 @@ begin
                  clk           => clk,
                  rst           => rst,
                  wr_en         => wr_en,
-                 mux_selector  => mux_selector,
+                 r_data_1_mux_selector  => r_data_1_mux_selector,
+                 r_data_2_mux_selector  => r_data_2_mux_selector,
                  rb_address1   => rb_address1,
                  rb_address2   => rb_address2,
                  rb_wr_reg     => rb_wr_reg,
-                 ula_mux_input => ula_mux_input,
+                 mux_1_input => mux_1_input,
                  ula_selector  => ula_selector,
                  ula_carry     => ula_carry,
                  ula_overflow  => ula_overflow,
@@ -99,8 +104,8 @@ begin
         wait for period_time * 2;
         rb_address1 <= "000";
         rb_address2 <= "000";
-        ula_mux_input <= X"0004";
-        mux_selector <= '1';
+        mux_1_input <= X"0004";
+        r_data_1_mux_selector <= '1';
         rb_wr_reg <= "001";
         wr_en <= '1';
         ula_selector <= "00";
@@ -108,8 +113,8 @@ begin
 
         wait for 100 ns;
         wr_en <= '0';
-        ula_mux_input <= x"0020";
-        mux_selector <= '1';
+        mux_1_input <= x"0020";
+        r_data_1_mux_selector <= '1';
         rb_wr_reg <= "010";
         wr_en <= '1';
         ula_selector <= "00";
@@ -121,13 +126,13 @@ begin
         rb_address2 <= "010";
         wr_en <= '1';
         rb_wr_reg <= "011";
-        mux_selector <= '0';
+        r_data_1_mux_selector <= '0';
         ula_selector <= "00";
 
         wait for 100 ns;
         wr_en <= '0';
-        mux_selector <= '1';
-        ula_mux_input <= X"0024";
+        r_data_1_mux_selector <= '1';
+        mux_1_input <= X"0024";
         rb_address1 <= "011";
         ula_selector <= "01";
         rb_wr_reg <= "011";
@@ -136,7 +141,7 @@ begin
 
         wait for 100 ns;
         wr_en <= '0';
-        ula_mux_input <= X"0000";
+        mux_1_input <= X"0000";
         rb_address1 <= "000";
         rb_address2 <= "000";
 
