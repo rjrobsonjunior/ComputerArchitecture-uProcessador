30a13b68a8ae7bb6f71c608913e3778fc883381a
Murilo Unten <murilo.unten@proton.me>
2024-05-18 12:57:53 -0300

    change top_level_tb to processador_tb

--- processador_tb.vhd
+++ processador_tb.vhd
@@ -2,10 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity Top_Level_tb is
+entity processador_tb is
 end entity;
 
-architecture rtl of Top_Level_tb is
+architecture rtl of processador_tb is
 
     component Top_Level is
         port(
